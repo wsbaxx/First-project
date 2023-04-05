53766fd8f410f683423e4eea5e0a19caa81425f9
Toma Kazusa <wsbaxx@163.com>
2023-04-05 11:13:35 +0800

    try1

---
+++ Homework01.java
@@ -0,0 +1,9 @@
+
+public class Homework01{
+	
+
+	public static void main(String[] args) {
+
+	  System.out.println("hello world");
+	}
+}
---
+++ Homework02.java
@@ -0,0 +1,9 @@
+
+public class Homework02{
+	
+
+	public static void main(String[] args) {
+
+	  System.out.println("姓名\t性别\t籍贯\t住址\n韦胜斌\t男\t广西贵港\t广东海洋大学");
+	}
+}
